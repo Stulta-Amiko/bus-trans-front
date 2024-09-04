f6880fa71575cd19d1363b7f33d00508727f1658
Stulta-Amiko <phh01010312@gmail.com>
2024-09-04 22:34:44 +0900

    스크립트 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [{
         name: 'bustransfront',
         script: 'node_modules/next/dist/bin/next',
-        args: 'start --port 80',
-        instances: 0,
+        args: 'start --port 8000',
+        instances: 1,
         exec_mode: 'cluster',
         wait_ready: true,
         kill_timeout: 5000,
